7d0dd087e184142817f35c23fc5fbd9a9f41ad81
aisys_cluster03_daisy12 <ksjmichael12@gmail.com>
2024-10-19 21:06:35 +0900

    cagra pruning

--- Jump/csrc/cagra_search.cu
+++ Jump/csrc/cagra_search.cu
@@ -4,36 +4,23 @@
 #include <cuda.h>
 #include <cuda_runtime.h>
 
+#include <cstdint>
+#include <iostream>
+
+
 namespace py = pybind11;
 
-__global__ void search_kernel(int *graph, int *queries, int *results, int topk)
+__global__ void search_kernel 
+(
+  int* graph,
+  int* queries,
+  int* results,
+  int topk
+)
 {
 
   const auto query_id = blockIdx.y;
-  
   extern __shared__ std::uint32_t smem[];
-
-
-  std::uint32_t result_buffer_size    = internal_topk + (search_width * graph_degree);
-  std::uint32_t result_buffer_size_32 = result_buffer_size;
-  if (result_buffer_size % 32) { result_buffer_size_32 += 32 - (result_buffer_size % 32); }
-  const auto small_hash_size = hashmap::get_size(small_hash_bitlen);
-
-  const auto query_smem_buffer_length =
-    raft::ceildiv<uint32_t>(dataset_desc.dim, DATASET_BLOCK_DIM) * DATASET_BLOCK_DIM;
-  auto query_buffer          = reinterpret_cast<QUERY_T*>(smem);
-  auto result_indices_buffer = reinterpret_cast<INDEX_T*>(query_buffer + query_smem_buffer_length);
-  auto result_distances_buffer =
-    reinterpret_cast<DISTANCE_T*>(result_indices_buffer + result_buffer_size_32);
-  auto visited_hash_buffer =
-    reinterpret_cast<INDEX_T*>(result_distances_buffer + result_buffer_size_32);
-  auto parent_list_buffer = reinterpret_cast<INDEX_T*>(visited_hash_buffer + small_hash_size);
-  auto distance_work_buffer_ptr =
-    reinterpret_cast<std::uint8_t*>(parent_list_buffer + search_width);
-  auto topk_ws        = reinterpret_cast<std::uint32_t*>(distance_work_buffer_ptr +
-                                                  DATASET_DESCRIPTOR_T::smem_buffer_size_in_byte);
-  auto terminate_flag = reinterpret_cast<std::uint32_t*>(topk_ws + 3);
-  auto smem_work_ptr  = reinterpret_cast<std::uint32_t*>(terminate_flag + 1);
 
   // Set smem working buffer for the distance calculation
   dataset_desc.set_smem_ptr(distance_work_buffer_ptr);
@@ -50,15 +37,9 @@ __global__ void search_kernel(int *graph, int *queries, int *results, int topk)
     topk_ws[0]        = ~0u;
   }
 
-  // Init hashmap
-  INDEX_T* local_visited_hashmap_ptr;
-  if (small_hash_bitlen) {
-    local_visited_hashmap_ptr = visited_hash_buffer;
-  } else {
-    local_visited_hashmap_ptr = visited_hashmap_ptr + (hashmap::get_size(hash_bitlen) * query_id);
-  }
-  hashmap::init(local_visited_hashmap_ptr, hash_bitlen, 0);
-  __syncthreads();
+  // init hashmap need hashmap
+  //
+  //
 
   // compute distance to randomly selecting nodes
   const INDEX_T* const local_seed_ptr = seed_ptr ? seed_ptr + (num_seeds * query_id) : nullptr;
@@ -164,14 +145,6 @@ __global__ void search_kernel(int *graph, int *queries, int *results, int topk)
                                                          search_width);
     }
 
-    // restore small-hash table by putting internal-topk indices in it
-    if ((iter + 1) % small_hash_reset_interval == 0) {
-      const unsigned first_tid = ((blockDim.x <= 32) ? 0 : 32);
-      hashmap_restore(
-        local_visited_hashmap_ptr, hash_bitlen, result_indices_buffer, internal_topk, first_tid);
-    }
-    __syncthreads();
-
     if (*terminate_flag && iter >= min_iteration) { break; }
 
     // compute the norms between child nodes and query node
@@ -191,54 +164,11 @@ __global__ void search_kernel(int *graph, int *queries, int *results, int topk)
       metric);
     __syncthreads();
 
-    // Filtering
-    if constexpr (!std::is_same<SAMPLE_FILTER_T,
-                                raft::neighbors::filtering::none_cagra_sample_filter>::value) {
-      if (threadIdx.x == 0) { *filter_flag = 0; }
-      __syncthreads();
+    // filtering
+    //
+    //
 
-      constexpr INDEX_T index_msb_1_mask = utils::gen_index_msb_1_mask<INDEX_T>::value;
-      const INDEX_T invalid_index        = utils::get_max_value<INDEX_T>();
-
-      for (unsigned p = threadIdx.x; p < search_width; p += blockDim.x) {
-        if (parent_list_buffer[p] != invalid_index) {
-          const auto parent_id = result_indices_buffer[parent_list_buffer[p]] & ~index_msb_1_mask;
-          if (!sample_filter(query_id, parent_id)) {
-            // If the parent must not be in the resulting top-k list, remove from the parent list
-            result_distances_buffer[parent_list_buffer[p]] = utils::get_max_value<DISTANCE_T>();
-            result_indices_buffer[parent_list_buffer[p]]   = invalid_index;
-            *filter_flag                                   = 1;
-          }
-        }
-      }
-      __syncthreads();
-    }
     iter++;
-  }
-
-  // Post process for filtering
-  if constexpr (!std::is_same<SAMPLE_FILTER_T,
-                              raft::neighbors::filtering::none_cagra_sample_filter>::value) {
-    constexpr INDEX_T index_msb_1_mask = utils::gen_index_msb_1_mask<INDEX_T>::value;
-    const INDEX_T invalid_index        = utils::get_max_value<INDEX_T>();
-
-    for (unsigned i = threadIdx.x; i < internal_topk + search_width * graph_degree;
-         i += blockDim.x) {
-      const auto node_id = result_indices_buffer[i] & ~index_msb_1_mask;
-      if (node_id != (invalid_index & ~index_msb_1_mask) && !sample_filter(query_id, node_id)) {
-        result_distances_buffer[i] = utils::get_max_value<DISTANCE_T>();
-        result_indices_buffer[i]   = invalid_index;
-      }
-    }
-
-    __syncthreads();
-    topk_by_bitonic_sort_1st<MAX_ITOPK + MAX_CANDIDATES>(
-      result_distances_buffer,
-      result_indices_buffer,
-      internal_topk + search_width * graph_degree,
-      top_k,
-      false);
-    __syncthreads();
   }
 
   for (std::uint32_t i = threadIdx.x; i < top_k; i += blockDim.x) {
@@ -269,10 +199,10 @@ torch::Tensor search(torch::Tensor graph, torch::Tensor queries) {
   dim3 block_dims(1, num_queries, 1);
 
   search_kernel<<<block_dims, thread_dims>>>(
-      graph.data_ptr<int>(),
-      queries.data_ptr<int>(),
-      results.data_ptr<int>(),
-      topk
+    graph.data_ptr<int>(),
+    queries.data_ptr<int>(),
+    results.data_ptr<int>(),
+    topk
   );  
 
   return results;
